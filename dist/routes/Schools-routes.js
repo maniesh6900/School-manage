c86b65abb48d122d07b0fa58bfcf63788756bfb7
maniesh6900 <kaj882626@gmail.com>
2025-03-08 19:11:44 +0530

    remove some file

--- dist/routes/Schools-routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-const School_controller_1 = require("../controller/School-controller");
-router.route("/create").post(School_controller_1.createSchool);
-router.route("/get").get(School_controller_1.getSchools);
-exports.default = router;
