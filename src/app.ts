3e76c420644c44577bab72757233e1d8ad83e101
maniesh6900 <kaj882626@gmail.com>
2025-03-08 19:40:08 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
 import express from 'express';
 const app = express();
+import cors from "cors";
 // import json so i can get data from post method
 app.use(express.json());
+
+// cors
+app.use(cors({
+    origin: "*",
+    }),
+);
 
 // import school routes for school
 import SchoolsRoutes from "./routes/Schools-routes";
 
 // init school routes
+app.get("/", (req, res)=>{
+    res.json({msg : "hello world"});
+});
+
+
 app.use("/api/v1/school", SchoolsRoutes);
 
 
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,17 @@ import dotenv from "dotenv";
 import { app } from "./app";
 const port = 4000 ;
 
+
 // config  env so a can use enc in application 
 dotenv.config({
     path :"./.env",
 });
 
-app.get("/", (req, res)=>{
-    res.json({msg : "hello world"});
-});
+
 
 // server listening 
 app.listen(port, ()=> {
     console.log(`server is up on : ${port}`);
-    
 });
+
+export {app};
